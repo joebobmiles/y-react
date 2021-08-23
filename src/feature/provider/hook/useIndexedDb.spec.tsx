71f7258a19b41462e051601edf06038301546641
Joseph R Miles <joe@jrm.dev>
2021-08-23 10:06:23 -0700

    test: mocked IndexeddbPersistance class

--- src/feature/provider/hook/useIndexedDb.spec.tsx
+++ src/feature/provider/hook/useIndexedDb.spec.tsx
@@ -1,18 +1,25 @@
 import { renderHook } from '@testing-library/react-hooks'
-import 'fake-indexeddb/auto'
 
 import * as Y from 'yjs'
 import { IndexeddbPersistence } from 'y-indexeddb'
 
 import { useIndexedDb } from './useIndexedDb'
 
+jest.mock('y-indexeddb', () => ({
+  IndexeddbPersistence: class {
+    whenSynced = new Promise<void>((resolve) => resolve())
+  }
+}))
+
 describe('useIndexedDb', () => {
-  it('Returns an IndexedDB provider', () => {
+  it('Returns an IndexedDB provider', async () => {
     const doc = new Y.Doc()
 
     const { result } = renderHook(
       () => useIndexedDb(doc, 'test')
     )
+
+    await result.current.whenSynced
 
     expect(result.current).toBeInstanceOf(IndexeddbPersistence)
   })
